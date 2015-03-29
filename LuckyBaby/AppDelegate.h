4300e08ac0548e48787f5893533035294596c8f3
Achilles <huangjiahong@huangjiahongdeiMac.local>
2015-03-29 09:06:00 +0800

    Initial Commit

---
+++ LuckyBaby/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LuckyBaby
+//
+//  Created by 黄嘉宏 on 15-3-29.
+//  Copyright (c) 2015年 黄嘉宏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LuckyBaby/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LuckyBaby
+//
+//  Created by 黄嘉宏 on 15-3-29.
+//  Copyright (c) 2015年 黄嘉宏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
